f5c2e553ea8da50c93c6cc2ee819e853cf7ee9b5
Jesstern Rays <jsstrn@users.noreply.github.com>
2026-06-13 02:43:33 +0800

    Add student name to output

--- modules/cohort/outputs.tf
+++ modules/cohort/outputs.tf
@@ -1,11 +1,18 @@
 data "aws_iam_account_alias" "current" {}
 
+locals {
+  account_alias  = data.aws_iam_account_alias.current.account_alias
+  login_url      = "https://${local.account_alias}.signin.aws.amazon.com/console"
+  login_profiles = aws_iam_user_login_profile.students
+}
+
 output "students" {
   value = {
-    for user, profile in aws_iam_user_login_profile.students : user => {
-      username  = profile.user
-      login_url = "https://${data.aws_iam_account_alias.current.account_alias}.signin.aws.amazon.com/console"
-      password  = profile.password
+    for s in local.students : s.aws_username => {
+      name      = s.name
+      username  = s.aws_username
+      login_url = local.login_url
+      password  = local.login_profiles[s.aws_username].password
     }
   }
   sensitive = true
