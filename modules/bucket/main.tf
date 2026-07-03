7b49a44f9ca8153957fd08afd1e239e13e5a9194
Jesstern Rays <jsstrn@users.noreply.github.com>
2026-07-04 00:55:44 +0800

    Fix bucket module configuration
    - Add required principal argument
    - Add bucket public access block
    - Add dependencies

--- modules/bucket/main.tf
+++ modules/bucket/main.tf
@@ -1,7 +1,31 @@
-data "aws_iam_policy_document" "deny_student_delete" {
+resource "aws_s3_bucket" "this" {
+  bucket        = var.bucket_name
+  force_destroy = var.force_destroy
+
+  tags = {
+    Cohort = "sctp-cloud-${var.cohort_code}"
+
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+data "aws_iam_policy_document" "deny_bucket_deletion" {
   statement {
-    sid    = "DenyStudentDeleteBucketAndObjects"
+    sid    = "DenyDeleteBucketAndObjects"
     effect = "Deny"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
 
     actions = [
       "s3:DeleteBucket",
@@ -22,17 +46,13 @@ data "aws_iam_policy_document" "deny_student_delete" {
   }
 }
 
-resource "aws_s3_bucket" "this" {
-  bucket        = var.bucket_name
-  force_destroy = var.force_destroy
-
-  tags = {
-    Cohort = "sctp-cloud-${var.cohort_code}"
-
-  }
-}
 
 resource "aws_s3_bucket_policy" "this" {
   bucket = aws_s3_bucket.this.id
-  policy = data.aws_iam_policy_document.deny_student_delete.json
+  policy = data.aws_iam_policy_document.deny_bucket_deletion.id
+
+  depends_on = [
+    aws_s3_bucket.this,
+    aws_s3_bucket_public_access_block.this
+  ]
 }
